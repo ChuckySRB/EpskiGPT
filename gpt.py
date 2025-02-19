0ebb743622179c21823a7cedf7d4e99e70317c46
Časlav Petronić <chucky.srb@gmail.com>
2025-02-19 21:18:18 +0800

    Removed leftouver code

--- gpt.py
+++ gpt.py
@@ -31,19 +31,6 @@ def decode(l, itos):
     return ''.join([itos[i] for i in l])
 
 
-@torch.no_grad()
-def estimate_loss():
-    out = {}
-    model.eval()
-    for split in ['train', 'val']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            X, Y = get_batch(split)
-            logits, loss = model(X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
 
 class AttentionHead(nn.Module):
     """ one head of self-attention """
